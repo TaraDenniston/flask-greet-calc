4e18f80607a67a072a9663a47b92df92b882cea6
Tara Denniston <taradenniston@gmail.com>
2022-11-04 20:09:18 -0400

    completed greet app

--- greet/app.py
+++ greet/app.py
@@ -1,3 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/welcome')
+def welcome_msg():
+    return 'welcome'
+
+@app.route('/welcome/home')
+def home_msg():
+    return 'welcome home'
+
+@app.route('/welcome/back')
+def back_msg():
+    return 'welcome back'
 
 
 
